2cb82af44fb40d4610b36f2266d4f96dd3ad80b8
azogue99 <arna1af@gmail.com>
2024-03-21 09:13:39 +0100

    update

--- Prj_Uf4/src/Main.java
+++ Prj_Uf4/src/Main.java
@@ -1,11 +1,16 @@
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.gson.Gson;
+import com.google.common.reflect.TypeToken;
+import java.lang.reclect.Type;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
 
-        String token = "EUyyFWixG6ynnTq9smPseIMB9oExRRz8U5";
+        String token = "EUnWcbuuw5cxlNGcaO8z1RkSIo2PEo5jdW";
 
         String CONNECT_API_URL = "https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=" + token;
 
@@ -17,5 +22,11 @@ public class Main {
         String fichero = br.readLine();
         br.close();
         System.out.println(fichero);
+
+
+        Type mapType = new TypeToken<Map<String, Map>>(){}.getType();
+        Map<String, String[]> son = new Gson().fromJson(fichero, mapType);
+
+
     }
 }
