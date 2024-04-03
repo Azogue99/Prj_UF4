862e7118455d795961d66a18aa7f657c71a3a9c4
Azogue - Omen <arna1af@gmail.com>
2024-04-03 14:16:13 +0200

    ya funciona pero hay que tener en cuenta las paginas.

--- Prj_Uf4/src/Main.java
+++ Prj_Uf4/src/Main.java
@@ -8,7 +8,6 @@ import org.json.*;
 public class Main {
 
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
-        System.out.println("0");
         // Llamamos al fichero generarCode.bat para generar el token token.json.
         String rutaBat = "generarCode.bat";
         ProcessBuilder jsonTokenGetter = new ProcessBuilder("cmd.exe", "/c", rutaBat); // lo ejecutamos con cmd.
@@ -16,31 +15,25 @@ public class Main {
 
         // Se comprueba que se haya ejecutado correctamente
         if (exe.waitFor() == 0) { // el waitFor tiene la capacidad de lanzar una InterruptedExeption
-            System.out.println("ok");
+            System.out.println("OK!");
         }else{
-            System.out.println("no s'ha executat el fitxer .bat correctament");
+            System.out.println("ERROR: no s'ha pogut executar el fitxer .bat correctament");
         }
-
-        System.out.println("1");
     
-        // Abrimos el fichero token.json y lo leemos
+        // Leemos del fichero json
         File json = new File("token.json");
         Scanner sc = new Scanner(json);
         String token = sc.nextLine();
         sc.close();
 
-        readToken(token);
-        System.out.println(token);
-
-        // hay que fixear esta expresion, no podemos iterar sobre caracteres
-        String subString = token.substring(17,51);
-        System.out.println(subString);
-
-        System.out.println("2");
+        // Obten el token del json
+        JSONObject tokenJsonObject = new JSONObject(token);
+        String sToken = tokenJsonObject.getString("access_token");
+        System.out.println("TOKEN: " + sToken);
 
         // esto es correcto
-        String CONNECT_API_URL = "https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=" + subString;
-        System.out.println(CONNECT_API_URL);
+        String CONNECT_API_URL = "https://us.api.blizzard.com/hearthstone/cards?locale=es_ES&page=1&pageSize=500&access_token=" + sToken;
+        System.out.println("CONNECTANT: " + CONNECT_API_URL);
         URL url = new URI(CONNECT_API_URL).toURL(); // tiene la capacidad de lanzar una URISyntaxException
 
         URLConnection urlc = url.openConnection();
@@ -53,8 +46,6 @@ public class Main {
         }
         br.close();
 
-        System.out.println("3");
-
         String jsonResponse = responseBuilder.toString();
 
         JSONObject jsonObject = new JSONObject(jsonResponse);
@@ -66,6 +57,7 @@ public class Main {
             String cardName = card.getString("name");
             System.out.println("Card Name: " + cardName);
         }
+        System.out.println(cardsArray.length());
     }
 
 
